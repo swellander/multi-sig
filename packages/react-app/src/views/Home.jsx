5ccc07c8a24e748b3c40941184f33714785e3950
Sam Wellander <samwellander@gmail.com>
2022-01-27 16:41:55 -0800

    Fix formatting

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -1,27 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { Grid, TextField, Button, Chip } from '@mui/material';
-// import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
-import Address from '../components/Address';
-import { useHistory } from 'react-router-dom';
+import styled from "styled-components";
+import Address from "../components/Address";
+import { Grid, TextField, Button, Chip } from "@mui/material";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import { useHistory } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
+const PadGrid = styled(Grid)`
+  padding: 20px;
+`;
+
+const FormItem = styled(Grid)`
+  margin: 10px;
+`;
+
+const AddressChip = styled(Chip)`
+  .MuiChip-label {
+    font-size: 16px;
+  }
+`;
 
 function Home({ readContracts, writeContracts }) {
   const history = useHistory();
   const { MultiSigWalletFactory } = readContracts;
   const [wallets, setWallets] = useState([]);
   const [owners, setOwners] = useState([]);
-  const [newOwner, setNewOwner] = useState('');
-  const [numConfirmations, setNumConfirmations] = useState(0);
+  const [newOwner, setNewOwner] = useState("");
+  const [numConfirmations, setNumConfirmations] = useState();
+
+  const createBtnIsDisabled = !numConfirmations || !owners.length;
 
   const handleAddOwner = () => {
     setOwners([...owners, newOwner]);
-    setNewOwner('');
-  }
+    setNewOwner("");
+  };
   const handleCreateContract = async () => {
     const txData = await writeContracts.MultiSigWalletFactory.create(owners, numConfirmations);
-    console.log({txData})
-  }
-  const handleClickWallet = walletAddress => history.push('/wallet/' + walletAddress);
+    console.log({ txData });
+  };
+  const handleClickWallet = walletAddress => history.push("/wallet/" + walletAddress);
 
   useEffect(async () => {
     const existingWallets = await readContracts.MultiSigWalletFactory?.getWallets();
@@ -29,42 +47,63 @@ function Home({ readContracts, writeContracts }) {
   }, [MultiSigWalletFactory]);
 
   return (
-    <Grid container>
-      <Grid xs={6} container>
-        <h3>Existing Wallets</h3>
-        {wallets.map(wallet => (
-          <Grid xs={12}>
-            <Chip
-              // icon={<AccountBalanceWalletIcon/>}
-              label={wallet}
-              onClick={() => handleClickWallet(wallet)}
-            />
+    <PadGrid container justifyContent="space-around">
+      <PadGrid xs={7} container>
+        <PadGrid xs={12} container justifyContent="flex-start" >
+          <h3>Create New Multi-Sig Wallet</h3>
+        </PadGrid>
+        <FormItem xs={12} container justifyContent="flex-start">
+          <Grid container xs={11}>
+            <TextField fullWidth label="Contract Owner" value={newOwner} onChange={e => setNewOwner(e.target.value)} />
           </Grid>
-        ))}
-      </Grid>
-      <Grid xs={6}>
-        <h3>Create Multi-Sig Wallet</h3>
-        <Grid>
-        <TextField
-          type="number"
-          value={numConfirmations}
-          onChange={e => setNumConfirmations(e.target.value)}
-        />
+          <Grid xs={1}>
+            <IconButton onClick={handleAddOwner}>
+              <AddCircleIcon fontSize="large" />
+            </IconButton>
+          </Grid>
+        </FormItem>
+        <Grid xs={4}>
+          <FormItem xs={12} container justifyContent="flex-start">
+            <TextField
+              label="Confirmations"
+              type="number"
+              value={numConfirmations}
+              onChange={e => setNumConfirmations(e.target.value)}
+            />
+          </FormItem>
+          <FormItem xs={12} container justifyContent="flex-start">
+            <Button variant="contained" onClick={handleCreateContract} disabled={createBtnIsDisabled}>
+              Create Contract
+            </Button>
+          </FormItem>
         </Grid>
-        <Grid>
-        <TextField
-          value={newOwner}
-          onChange={e => setNewOwner(e.target.value)}
-        />
-        <Button
-          onClick={handleAddOwner}
-        >Add Owner</Button>
+        <Grid xs={7} style={{ overflow: "scroll", margin: 10, height: '40vh'}}>
+          {owners.map(owner => (
+            <Grid xs={12} container justifyContent="flex-start" style={{marginBottom: 10}}>
+              <Address fontSize={16} address={owner} size="long" />
+            </Grid>
+          ))}
         </Grid>
-        <Button onClick={handleCreateContract}>Create Contract</Button>
-      </Grid>
-      <h3>Owners</h3>
-      {owners.map(owner => <Address address={owner}/>)}
-    </Grid>
+      </PadGrid>
+
+      <PadGrid xs={4}>
+        <PadGrid xs={12} container justifyContent="flex-start">
+          <h3>Existing Multi Sig Wallets</h3>
+        </PadGrid>
+        <Grid>
+          {wallets.map(wallet => (
+            <PadGrid xs={12} container justifyContent="flex-start" style={{ padding: 5 }}>
+              <AddressChip
+                fontSize={16}
+                icon={<AccountBalanceWalletIcon />}
+                label={wallet}
+                onClick={() => handleClickWallet(wallet)}
+              />
+            </PadGrid>
+          ))}
+        </Grid>
+      </PadGrid>
+    </PadGrid>
   );
 }
 
